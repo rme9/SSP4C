af8be462038d46ef2c12348bf6eb929a26f747e4
Rebecca Elliff <rme9@students.uwf.edu>
2017-11-28 00:32:04 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+let app = express();
+
+let storeService = require('./lib/storeService.js');
+let bodyParser = require('body-parser');
 
 // set up handlebars view engine
-var handlebars = require('express-handlebars')
+let handlebars = require('express-handlebars')
 	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -11,10 +14,50 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('body-parser').json());
 
 app.get('/', function(req, res) {
 	res.render('home');
+});
+
+app.get('/viewStores', function(req, res){
+	storeService.getAllStores().then(function(results){
+		res.render('store', {stores:results});
+	});
+});
+
+let stres; // Used for McHackery becuase this is doing something wierd
+app.get('/viewStore/:id', function(req, res){
+		storeService.getStoresbyId(req.params.id).then(function(result){
+			if(result.length > 0)
+			{
+				stres = result;
+			}
+			res.render('storedetail', {store:stres[0]});
+		});
+});
+
+app.post('/saveStore/:id', function(req, res) {
+	storeService.updateStore(req.body, req.params.id).then(function(result) {
+		res.redirect('/viewStore/'+req.params.id);
+	})
+});
+
+app.post('/saveStore', function(req, res) {
+	storeService.saveStore(req.body).then(function(result){
+		res.redirect('/viewStores');
+	});
+});
+
+app.get('/itemSearch', function(req, res) {
+	res.render('itemsearch', {stores:{}, itemSearched:""});
+});
+
+app.post('/search', function(req, res) {
+	storeService.itemSearch(req.body).then(function(results) {
+			res.render('itemsearch', {stores:results, itemSearched:req.body.name});
+	});
 });
 
 // 404 catch-all handler (middleware)
